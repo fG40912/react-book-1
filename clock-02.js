14938d9a5fe57b8c44e95c548321337606a73afb
fG40912 <futureg412@gmail.com>
2023-02-04 12:40:11 +1030

    high-order functions with currying technique

--- clock-02.js
+++ clock-02.js
@@ -27,3 +27,23 @@ const appendAMPM = time => ({
     ...time,
     AMPM: time.hours >= 12 ? "PM" : "AM"
 })
+
+// function: display
+// Takes a target function and returns a function that will send a time to the target. In this example, target will be console.log.
+
+const display = target => time => target(time)
+
+// function: formatClock
+// Takes a template string and uses it to return clock time formatted based on the criteria from the string. In this example, the template is "hh:mm:ss tt". From there, formatClock will replace the placeholders with hours, minutes, seconds, time of the day.
+
+const formatClock = format => time => {
+    format.replace("hh",time.hours).replace("mm",time.minutes).replace("ss",time.seconds).replace("tt",time.AMPM)
+}
+
+// function: prependZero
+// Takes an object's key as an argument and prepends a zero to the value stored under that object's key. It takes in a key to a specific field and prepends values with a zero if the value is less than 10.
+
+const prependZero = key = time => ({
+    ...time,
+    key: time[key] < 10 ? "0" + time[key].toString() : time[key].toString()
+})
