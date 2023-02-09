e321f47f105cd252f994b212bcfed58bc5683d1f
fG40912 <futureg412@gmail.com>
2023-02-09 10:53:17 +1030

    Sending Interactions Back up a Component Tree

--- five-star-app/src/App.js
+++ five-star-app/src/App.js
@@ -3,11 +3,22 @@ import colorData from "./color-data.json"
 import ColorList from "./ColorList"
 
 function App() {
-	const [colors, setColors] = useState(colorData) 
+	const [colors, setColors] = useState(colorData)
+
+	function handleRemove(id){
+		const newColors = colors.filter(color => color.id!==id)
+		setColors(newColors)
+	}
+
+	function handleRating(id,rating){
+		const newColors = colors.map(color => color.id === id ? { ...color, rating } : color);
+		setColors(newColors)
+	}
+
     return (
 		<>
 			<h1>Hello World</h1>
-			<ColorList colors={colors}/>
+			<ColorList colors={colors} onRemove={handleRemove} onRateColor={handleRating}/>
 		</>
     );
 }
--- five-star-app/src/StarRating.js
+++ five-star-app/src/StarRating.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Star from "./Star"
 
 const createArray = length => [...Array(length)]
 
-export default function StarRating({totalLength=5, selectedStars=0}){
+export default function StarRating({totalLength=5, selectedStars=0, onRate= f=>f}){
     return(
         <>
-            {createArray(totalLength).map((n,i) => <Star key={i} selected={selectedStars > i}/>)}
+            {createArray(totalLength).map((n,i) => <Star key={i} selected={selectedStars > i} onSelect={() => onRate(i+1)}/>)}
             <p>{selectedStars} out of {totalLength}</p>
         </>
     )
