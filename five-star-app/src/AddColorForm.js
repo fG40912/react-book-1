6595ba73231e9e2f35c7007b565e6f5e347d23ff
fG40912 <futureg412@gmail.com>
2023-02-09 15:28:45 +1030

    Custom Hooks

--- five-star-app/src/AddColorForm.js
+++ five-star-app/src/AddColorForm.js
@@ -1,20 +1,21 @@
-import React, {useState} from "react";
+import React from "react";
+import useInput from "./useInput";
 
 export default function AddColorForm({onNewColor=f=>f}){
-    const [title, setTitle] = useState("")
-    const [color, setColor] = useState("#000000")
+    const [titleProps, resetTitle] = useInput("")
+    const [colorProps, resetColor] = useInput("#000000")
 
     const submit = e => {
         e.preventDefault()
-        onNewColor(title,color)
-        setTitle("")
-        setColor("")
+        onNewColor(titleProps.value,colorProps.value)
+        resetTitle()
+        resetColor()
     }
 
     return(
         <form onSubmit={submit}>
-            <input type="text" onChange={event => setTitle(event.target.value)} placeholder="color title ..." required/>
-            <input type="color" onChange={event => setColor(event.target.value)} required/>
+            <input type="text" {...titleProps} placeholder="color title ..." required/>
+            <input type="color" {...colorProps} required/>
             <button>ADD</button>
         </form>
     )
