ab04d95185794046a9e80075eaea254750b1bd40
fG40912 <futureg412@gmail.com>
2023-02-10 17:27:44 +1030

    Stateful Provider

---
+++ five-star-app/src/ColorProvider.js
@@ -0,0 +1,31 @@
+import React, {createContext, useState} from "react";
+import colorsData from "./color-data.json"
+import { v4 } from "uuid"
+
+const ColorContext = createContext()
+
+export default function ColorProvider({children}){
+    const [colors,setColors] = useState(colorsData)
+
+    const addColor = (title,color) =>{
+        const newColors = [...colors,{id:v4(),rating:0,title,color}]
+        setColors(newColors)
+    }
+
+    const rateColor = (id,rating) => {
+        setColors(colors.map((color,index)=>{
+            if(index === id) color.rating = rating
+        }))
+    }
+
+    const removeColor = (id) => {
+        const newColors = colors.filter(color => color.id !== id)
+        setColors(newColors)
+    }
+
+    return(
+        <ColorContext.Provider value={{colors,addColor,rateColor,removeColor}}>
+            {children}
+        </ColorContext.Provider>
+    )
+}
