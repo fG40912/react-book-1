6f1621a75cccec1816a52aee7d2e31afe258417d
fG40912 <futureg412@gmail.com>
2023-02-14 18:09:16 +1030

    saving Data Locally

--- github_app/src/App.js
+++ github_app/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+const saveJSON = (key, data) => {
+	localStorage.setItem(key,JSON.stringify(data))
+}
+
+const loadJSON = key => {
+	return key && JSON.parse(localStorage.getItem(key))
+}
+
+function GithubUser({ login }){
+	const [data, setData] = useState(loadJSON(`user:${login}`))
+
+	useEffect(
+		() => {
+			if(!data) return
+			if(data.login === login) return
+
+			const { name, avatar_url, location } = data
+			saveJSON(`user:${login}`,
+				{
+					name,
+					login,
+					avatar_url,
+					location
+				}
+			)
+		},[data]
+	)
+
+	useEffect(
+		() => {
+			if(!login) return
+			if(data && data.login === login) return	// complain about data.login cannot be accessed
+			else{
+				fetch(`https://api.github.com/users/${login}`).then(response => response.json()).then(setData).catch(console.error)
+			}
+		},[login]
+	)
+
+	if(data) return(<pre>{ JSON.stringify(data) }</pre>)
+	else return null
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<GithubUser login={ "moonhighway" }/>
+	);
 }
 
 export default App;
