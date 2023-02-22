72e4309045822a961ed1690777a1854fa0647069
FutureNoogler <futureg412@gmail.com>
2023-02-22 00:32:18 +0000

    compile sample.js with eslint-plugin-react-hook

--- testing_router/src/sample.js
+++ testing_router/src/sample.js
@@ -1,16 +1,6 @@
-const gnar = "gnarly";
-
-const info = ({
-        file = __filename,
-        dir = __dirname
-    }) => (
-        <p>
-        {dir}: {file}
-        </p>
-);
-
-switch (gnar) {
-    default:
-        console.log("gnarly");
-        break;
+function gnar() {
+    const [nickname, setNickname] = useState(
+      "dude"
+    );
+    return <h1>gnarly</h1>;
 }
