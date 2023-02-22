b8b6b39d1542f16874c57a747db42bfbb524abbf
FutureNoogler <futureg412@gmail.com>
2023-02-22 23:31:18 +0000

    integrating prettier into eslint

--- testing_router/src/sample.js
+++ testing_router/src/sample.js
@@ -1,10 +1,1 @@
-function gnar() {
-    const [nickname, setNickname] = useState(
-      "dude"
-    );
-    return <h1>gnarly</h1>;
-}
-
-function Image() {
-    return <img src="/img.png" />;
-}
+console.log("Prettier Test")
