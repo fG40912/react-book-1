0a7ec5f0b2cdc289564df9a9700a305b7bb12be2
FutureNoogler <futureg412@gmail.com>
2023-02-26 10:01:58 +0000

    Nesting Routes not functioning

--- testing_router/src/App.js
+++ testing_router/src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Home, About, Events, Products, Contact, Whoops404 } from "./pages";
+import { useRoutes } from "react-router-dom";
+import {
+  Home,
+  About,
+  Events,
+  Products,
+  Contact,
+  Whoops404,
+  Services,
+  Location,
+  History
+} from "./pages";
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Whoops404 />} />
-      </Routes>
-    </div>
-  );
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    {
+      path: "about",
+      element: <About />,
+      children: [
+        { path: "services", element: <Services /> },
+        { path: "history", element: <History /> },
+        { path: "location", element: <Location /> }
+      ]
+    },
+    { path: "events", element: <Events /> },
+    { path: "products", element: <Products /> },
+    { path: "contact", element: <Contact /> },
+    { path: "*", element: <Whoops404 /> },
+    { path: "services", redirectTo: "about/services" }
+  ]);
+
+  return element;
 }
 
 export default App;
--- testing_router/src/pages.js
+++ testing_router/src/pages.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, Outlet } from "react-router-dom";
 
 export function Home() {
   return (
@@ -19,6 +19,7 @@ export function About() {
   return (
     <div>
       <h1>[About]</h1>
+      <Outlet />
     </div>
   );
 }
@@ -70,3 +71,81 @@ export function Services() {
     </p>
   </section>;
 }
+
+export const Location = () => (
+  <section className="location">
+    <h2>Our Location</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      Praesent libero. Sed cursus ante dapibus diam.
+    </p>
+    <p>
+      Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+      Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+      Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora
+      torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales
+      ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+      quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+      tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus
+      risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis
+      ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel,
+      tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+      aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos.
+    </p>
+  </section>
+);
+
+export const Company = () => (
+  <section className="company">
+    <h2>About the Company</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at
+      nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+      tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget
+      nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+      per inceptos himenaeos. Curabitur sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+      quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+      tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus
+      risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis
+      ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel,
+      tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+      aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos.
+    </p>
+  </section>
+);
+
+export const History = () => (
+  <section className="history">
+    <h2>Our History</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+      Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at
+      nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+      tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget
+      nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+      per inceptos himenaeos. Curabitur sodales ligula in libero.
+    </p>
+    <p>
+      Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+      quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+      tristique sem. Proin ut ligula vel nunc egestas porttitor.
+    </p>
+    <p>
+      {" "}
+      Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce
+      ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus,
+      ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat
+      condimentum velit. Class aptent taciti sociosqu ad litora torquent per
+      conubia nostra, per inceptos himenaeos.
+    </p>
+  </section>
+);
